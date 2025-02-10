b7b7a77aed3d8f207504d59063f40dd5f23a9922
JoaoPPagotto <pagotto.costa@gmail.com>
2025-02-10 17:20:27 -0300

    Fix - Erro pagina Financiador não aparecendo

--- src/models/Repositories/FinanciadorRepository.ts
+++ src/models/Repositories/FinanciadorRepository.ts
@@ -1,94 +1,93 @@
-import api from '@/api/api';
-import { Financiador, IFinanciador } from '../Entities/Financiador';
-import FinanciadorRoutes from '../ApiRoutes/FinanciadorRoutes';
+import api from '@/api/api'
+import type { IFinanciador } from '../Entities/Financiador'
+import { Financiador } from '../Entities/Financiador'
+import FinanciadorRoutes from '../ApiRoutes/FinanciadorRoutes'
 
 export default class FinanciadorRepository {
-  apiClient;
+  apiClient
   constructor() {
-    this.apiClient = api;
+    this.apiClient = api
   }
 
   createBaseRoute() {
-    return new FinanciadorRoutes({}).getAll;
+    return new FinanciadorRoutes({}).getAll
   }
 
   createDeleteRoute(id: number) {
-    return new FinanciadorRoutes({ id: id }).delete;
+    return new FinanciadorRoutes({ id: id }).delete
   }
 
-  async fetchAllFinanciador() {
+  async fetchFinanciador_es() {
     try {
-      // Criar rota de conex�o
-      const baseRoute = this.createBaseRoute();
+      // Criar rota de conexão
+      const baseRoute = this.createBaseRoute()
 
-      // Faz a request usando a api com o axios
-      const response = await this.apiClient.get(baseRoute);
+      // Faz a request usando a API com o axios
+      const response = await this.apiClient.get(baseRoute)
 
-      // Retorna a fun��o com a cria��o de objetos
-        return response.data.value.map((financiador: IFinanciador) =>
-        new Financiador (
-          financiador.Id,
-          financiador.Nome,
-          financiador.Email
-        ));
+      // Retorna a função com a criação de objetos
+      return response.data.$values.map((projeto: IFinanciador) => {
 
+        return new Financiador(
+          projeto.Id,
+          projeto.Nome,
+          projeto.Email
+        )
+      })
     } catch (error) {
-      console.error("Erro ao buscar financiadores.", error);
-      throw error;
+      console.error('Erro ao buscar financiadores.', error)
+      throw error
     }
   }
 
-    async createFinanciador(form: IFinanciador) {
+  async createFinanciador(form: IFinanciador) {
     try {
-      // Criar rota de conex�o
-      const baseRoute = this.createBaseRoute();
+      // Criar rota de conexão
+      const baseRoute = this.createBaseRoute()
 
       // Faz o post usando a api com o axios e enviando os dados
-      const response = await this.apiClient.post(baseRoute, form);
+      const response = await this.apiClient.post(baseRoute, form)
 
       // Retorna a resposta do backend
-      return response;
-
+      return response
     } catch (error) {
-      console.error("Erro ao criar financiador.", error);
-      throw error;
+      console.error('Erro ao criar financiador.', error)
+      throw error
     }
   }
 
-    async updateFinanciador(Id: number, form: IFinanciador) {
+  async updateFinanciador(id: number, form: IFinanciador) {
     try {
-      // Criar rota de conex�o
-      const baseRoute = this.createBaseRoute();
+      // Criar rota de conexão
+      const baseRoute = this.createBaseRoute()
 
-      // Garante que o Id est� salvo dentro do form
-      form.Id = Id;
+      // Garante que o Id está salvo dentro do form
+      form.Id = id
 
       // Faz o put usando a api com o axios e enviando os dados
-      const response = await this.apiClient.put(baseRoute, form);
+      const response = await this.apiClient.put(`${baseRoute}/${id}`, form)
 
       // Retorna a resposta do backend
-      return response;
-
+      return response
     } catch (error) {
-      console.error("Erro ao atualizar financiador.", error);
-      throw error;
+      console.error('Erro ao atualizar financiador.', error)
+      throw error
     }
   }
 
   async deleteFinanciador(Id: number) {
     try {
-      // Criar rota de conex�o
-      const deleteRoute = this.createDeleteRoute(Id);
+      // Criar rota de conexão
+      const deleteRoute = this.createDeleteRoute(Id)
 
       // Faz o delete usando a api com o axios e enviando os dados
-      const response = await this.apiClient.delete(deleteRoute);
+      const response = await this.apiClient.delete(deleteRoute)
 
       // Retorna a resposta do backend
-      return response;
-
+      return response
     } catch (error) {
-      console.error("Erro ao deletar o financiador.", error);
-      throw error;
+      console.error('Erro ao deletar o financiador.', error)
+      throw error
     }
   }
 }
